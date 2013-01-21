055c1e390218d3955eb6b656faad519bb70ff31e
Pete Michaud <michaudp@gmail.com>
2013-01-20 20:33:31 -0600

    resume interaction

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,14 @@
-console.log('This would be the main JS file.');
+$(document).ready(function(){
+    var $btn = $('.full-toggle-btn');
+
+    $btn.click(function(){
+       $('.summary, .experience ul, .education > p').slideToggle('fast');
+
+        var current_text = $btn.text();
+
+        $btn
+            .text($btn.data('toggle-anchor'))
+            .data('toggle-anchor', current_text);
+
+   });
+});
